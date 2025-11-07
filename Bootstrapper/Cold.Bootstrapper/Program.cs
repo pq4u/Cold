ebd32664f7156e98fbcb3692abcd9a7e4f46806e
pq4u <paweltes19@gmail.com>
2025-11-07 23:30:45 +0100

    Add extensions in Shared project

--- Bootstrapper/Cold.Bootstrapper/Program.cs
+++ Bootstrapper/Cold.Bootstrapper/Program.cs
@@ -1,6 +1,14 @@
+using Cold.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSharedFramework(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSharedFramework();
+
+app.MapControllers();
+app.MapGet("/ping", ctx => ctx.Response.WriteAsync("pong"));
 
 app.Run();
---
+++ Shared/Cold.Shared/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Cold.Shared;
+
+public static class Extensions
+{
+    private const string ApiTitle = "Cold API";
+    private const string ApiVersion = "v1";
+    
+    public static IServiceCollection AddSharedFramework(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen(s =>
+        {
+            s.EnableAnnotations();
+            s.CustomSchemaIds(x => x.FullName);
+            s.SwaggerDoc(ApiVersion, new()
+            {
+                Title = ApiTitle,
+                Version = ApiVersion
+            });
+        });
+        
+        return services;
+    }
+
+    public static IApplicationBuilder UseSharedFramework(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseReDoc(rd =>
+        {
+            rd.RoutePrefix = "docs";
+            rd.SpecUrl($"/swagger/{ApiVersion}/swagger.json");
+            rd.DocumentTitle = ApiTitle;
+        });
+
+        app.UseRouting();
+
+        return app;
+    }
+}
