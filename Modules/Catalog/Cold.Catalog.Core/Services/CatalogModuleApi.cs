412b2d0b3a05ac66d72c51d0bf2b8d10a5c8811d
pq4u <paweltes19@gmail.com>
2025-11-11 19:16:31 +0100

    Add getting product price interface in catalog module

--- Modules/Catalog/Cold.Catalog.Core/Services/CatalogModuleApi.cs
+++ Modules/Catalog/Cold.Catalog.Core/Services/CatalogModuleApi.cs
@@ -5,12 +5,17 @@ namespace Cold.Catalog.Core.Services;
 public class CatalogModuleApi : ICatalogModuleApi
 {
     private readonly IProductService _productService;
+    private readonly IProductPriceService _productPriceService;
 
-    public CatalogModuleApi(IProductService productService)
+    public CatalogModuleApi(IProductService productService, IProductPriceService productPriceService)
     {
         _productService = productService;
+        _productPriceService = productPriceService;
     }
 
     public async Task<IReadOnlyList<string>> GetProductsNamesAsync(List<Guid> productIds)
         => await _productService.GetNamesAsync(productIds);
+
+    public async Task<decimal?> GetProductPriceAsync(Guid productId, string classType, DateTimeOffset date)
+        => await _productPriceService.GetPriceAsync(productId, classType, date);
 }
--- Modules/Catalog/Cold.Catalog.Shared/ICatalogModuleApi.cs
+++ Modules/Catalog/Cold.Catalog.Shared/ICatalogModuleApi.cs
@@ -3,4 +3,5 @@
 public interface ICatalogModuleApi
 {
     Task<IReadOnlyList<string>> GetProductsNamesAsync(List<Guid> productIds);
+    Task<decimal?> GetProductPriceAsync(Guid productId, string classType, DateTimeOffset date);
 }
