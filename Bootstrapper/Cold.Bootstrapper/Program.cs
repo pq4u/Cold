468a8d87f0825e7ec00c23f24762c056f4673db1
pq4u <paweltes19@gmail.com>
2025-11-11 13:32:25 +0100

    Add contract module to bootstrapper

--- Bootstrapper/Cold.Bootstrapper/Program.cs
+++ Bootstrapper/Cold.Bootstrapper/Program.cs
@@ -1,15 +1,21 @@
 using Cold.Catalog.Api;
+using Cold.Contracts.Api;
 using Cold.Shared;
+using QuestPDF.Infrastructure;
+
+QuestPDF.Settings.License = LicenseType.Community;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddCatalogModule()
+    .AddContractsModule()
     .AddSharedFramework(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseCatalogModule();
+app.UseContractsModule();
 app.UseSharedFramework();
 
 app.MapControllers();
